7d7833a6fa81fe1c310b9bb32a3cc3d81c200c6d
Ruths2 <ruth09@yahoo.com>
2022-05-13 20:49:53 -0300

    Cria componente services/correntista e configura para se conectar com a API na maquina local(meu celular). No arquivo movimentacao-new.component.ts solicita a lista de correntistas da API. Formata o valor com a biblioteca ng2-currency-mask.

--- ProjetoSantander/bankline-app/src/app/components/movimentacao-new/movimentacao-new.component.ts
+++ ProjetoSantander/bankline-app/src/app/components/movimentacao-new/movimentacao-new.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CorrentistaService } from 'src/app/services/correntista.service';
+import { MovimentacaoService } from 'src/app/services/movimentacao.service';
 
 @Component({
   selector: 'app-movimentacao-new',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movimentacao-new.component.css']
 })
 export class MovimentacaoNewComponent implements OnInit {
+  correntistas:any;
+  correntista:any;
 
-  constructor() { }
+  constructor(
+    private movimentacaoService: MovimentacaoService,
+    private correntistaService: CorrentistaService,
+    ) { }
 
   ngOnInit(): void {
+    this.exibirCorrentistas();
+  }
+  exibirCorrentistas(): void {
+    this.correntistaService.list()
+      .subscribe(
+        dataHora => {
+          this.correntistas = dataHora;
+          console.log(dataHora);
+        },
+        error => {
+          console.log(error);
+        });
   }
 
 }
+
