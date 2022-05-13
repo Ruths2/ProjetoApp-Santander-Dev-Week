ae1e539be20c393f972c5a26f8a99a51daa67bae
Ruths2 <ruth09@yahoo.com>
2022-05-13 02:34:57 -0300

    Fiz tanta coisa que me perdi no commit kkkk, mas vamos la: Cria a tabela das movimentações e a integração com a api local atraves do componente services/movimentacao. Formata data/hora e valor com o Pipe. Modifica as cores do valor e cria botao nova movimentacao

--- ProjetoSantander/bankline-app/src/app/app.module.ts
+++ ProjetoSantander/bankline-app/src/app/app.module.ts
@@ -10,6 +10,11 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
@@ -20,9 +25,11 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- ProjetoSantander/bankline-app/src/app/components/movimentacao-list/movimentacao-list.component.ts
+++ ProjetoSantander/bankline-app/src/app/components/movimentacao-list/movimentacao-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MovimentacaoService } from 'src/app/services/movimentacao.service';
+
 
 @Component({
   selector: 'app-movimentacao-list',
@@ -9,9 +11,22 @@ export class MovimentacaoListComponent implements OnInit {
 
   movimentacoes:any;
 
-  constructor() { }
+  constructor(private movimentacaoService: MovimentacaoService,) { }
 
   ngOnInit(): void {
+    this.listMovimentacoes();
+  }
+
+  listMovimentacoes(): void {
+    this.movimentacaoService.list()
+    .subscribe(
+        data => {
+          this.movimentacoes = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
   }
 
 }
