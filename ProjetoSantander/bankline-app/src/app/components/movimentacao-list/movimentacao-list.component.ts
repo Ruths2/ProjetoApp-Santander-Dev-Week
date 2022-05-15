035a048cd58b41522fdad08105095107a71b4642
Ruths2 <ruth09@yahoo.com>
2022-05-14 22:10:58 -0300

    Cria o botão bara listar as movimentações por correntistas e seus metodos para buscar pelo id da Conta

--- ProjetoSantander/bankline-app/src/app/components/movimentacao-list/movimentacao-list.component.ts
+++ ProjetoSantander/bankline-app/src/app/components/movimentacao-list/movimentacao-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MovimentacaoService } from 'src/app/services/movimentacao.service';
-
+import { CorrentistaService } from 'src/app/services/correntista.service';
 
 @Component({
   selector: 'app-movimentacao-list',
@@ -10,15 +10,31 @@ import { MovimentacaoService } from 'src/app/services/movimentacao.service';
 export class MovimentacaoListComponent implements OnInit {
 
   movimentacoes:any;
+  correntista:any;
+  correntistas:any;
 
-  constructor(private movimentacaoService: MovimentacaoService,) { }
+  constructor(
+    private movimentacaoService: MovimentacaoService,
+    private correntistaService: CorrentistaService,) { }
 
   ngOnInit(): void {
-    this.listMovimentacoes();
+    this.exibirCorrentistas()
+  }
+
+ exibirCorrentistas(): void {
+    this.correntistaService.list()
+      .subscribe(
+        dataHora => {
+          this.correntistas = dataHora;
+          console.log(dataHora);
+        },
+        error => {
+          console.log(error);
+        });
   }
 
   listMovimentacoes(): void {
-    this.movimentacaoService.list()
+    this.movimentacaoService.findByIdConta(this.correntista.id)
     .subscribe(
         dataHora => {
           this.movimentacoes = dataHora;
