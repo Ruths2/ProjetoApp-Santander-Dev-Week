3d8eea9ba844fca8b076da361b06d885171eafb1
Ruths2 <ruth09@yahoo.com>
2022-06-20 04:53:01 -0300

    Cria as configurações necessarias para realizar o deploy no Heroku e modifica os components com o routerLink ao inves do href. Deploy funcionou, tudo ok graças a Deus :D

---
+++ ProjetoSantander/bankline-app/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const appName = 'bankline-app';
+const outputPath = `${__dirname}/dist/${appName}`;
+app.use(express.static(outputPath));
+app.get('/*', (req,res) => {
+  res.sendFile(`${outputPath}/index.html`);
+});
+app.listen(process.env.PORT);
